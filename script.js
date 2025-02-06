d01f243a50de8254bb1410856a314c049c82439c
funZ <153361103+PaFunzFr@users.noreply.github.com>
2025-02-06 11:20:15 +0100

    added animations for legendary pokemon

--- script.js
+++ script.js
@@ -29,7 +29,7 @@ async function fetchPokemonData(limit, offset) {
             pushIfLimitIsntReached(1000, pokemon);
         });
     } catch (error) {
-        console.error("Fehler beim Laden der Pokémon:", error);
+        console.error("Error while loading Poke API", error);
     }
 } 
 
@@ -77,7 +77,7 @@ async function fetchPokemonDetails(pokemonId) {
         const responseAsJson = await response.json();
         return responseAsJson;
     } catch (error) {
-        console.error("Fehler beim Laden der Pokémon-Details:", error);
+        console.error("Error while loading Poke API", error);
     }
 }
 
@@ -87,17 +87,18 @@ async function fetchPokemonSpecies(pokemonId) {
         const responseAsJson = await response.json();
         return responseAsJson;
     } catch (error) {
-        console.error("Fehler beim Laden der Pokémon-Spezies-Details:", error);
+        console.error("Error while loading Poke API", error);
     }
 }
 
+// currently not used
 async function fetchPokemonEncounters(pokemonId) {
     try {
         const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}/encounters`);
         const responseAsJson = await response.json();
         return responseAsJson;
     } catch (error) {
-        console.error("Fehler beim Laden der Pokémon-Spezies-Details:", error);
+        console.error("Error while loading Poke API", error);
     }
 }
 
@@ -114,13 +115,17 @@ async function loadMorePokemon() {
     document.getElementById("spinner").classList.remove("hidden");
     await fetchPokemonData(30, currentShownPokemon);
     await getPokemonInfos(allPokemonData.slice(currentShownPokemon, currentShownPokemon + 30));
+    renderDelayedCards();
+    loading = false;
+}
+
+function renderDelayedCards() {
     setTimeout(() => {
         renderPokemonCards(currentShownPokemon);
         currentShownPokemon += 30;
         document.getElementById("spinner").classList.add("hidden");
         enableScroll();
     }, 400);
-    loading = false;
 }
 
 function disableScroll() {
@@ -141,54 +146,6 @@ function openModal(pokemonId) {
     pokemonDetailModal.style.display ="flex";
 }
 
-function renderModal(index) {
-    return pokemonDetailContent.innerHTML = `
-    <h2>${allPokemonInfos[index].name}</h2>
-    <img class="modal-img"src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${allPokemonInfos[index].id}.png" alt="${allPokemonInfos[index].name}">
-    <div id="general-properties">
-    <div class="modal-infos">
-        <ul class="modal-categories">
-            <li onclick="showGeneralStats(${index})">Allgemein</li>
-            <li onclick="showAttributes(${index})">Eigenschaften</li>
-            <li onclick="showEvolutionChain(${index})">Entwicklungen</li>
-        </ul>
-        <div id="modalContent"></div>
-    </div>`;
-};
-
-function showAttributes(index) {
-    document.getElementById('modalContent').innerHTML = "";
-    const pokStats = allPokemonInfos[index].stats;
-    document.getElementById('modalContent').innerHTML = `
-    <ul class="modal-attributes">
-        <li>
-            <h3 class="attribute-title">Health:</h3>
-            <div class="attribute-bar" style="width: ${calculateBarWidth(pokStats[0].base_stat)}px;"></div>
-        </li>
-        <li>
-            <h3 class="attribute-title">Attack:</h3>
-            <div class="attribute-bar" style="width: ${calculateBarWidth(pokStats[1].base_stat)}px;"></div>
-        </li>
-        <li>
-            <h3 class="attribute-title">Defense:</h3>
-            <div class="attribute-bar" style="width: ${calculateBarWidth(pokStats[2].base_stat)}px;"></div>
-        </li>
-        <li>
-            <h3 class="attribute-title">Special-Atk:</h3>
-            <div class="attribute-bar" style="width: ${calculateBarWidth(pokStats[3].base_stat)}px;"></div>
-        </li>
-        <li>
-            <h3 class="attribute-title">Special-Def:</h3>
-            <div class="attribute-bar" style="width: ${calculateBarWidth(pokStats[4].base_stat)}px;"></div>
-        </li>
-        <li>
-            <h3 class="attribute-title">Speed:</h3>
-            <div class="attribute-bar" style="width: ${calculateBarWidth(pokStats[5].base_stat)}px;"></div>
-        </li>
-    </ul>
-    `;
-}
-
 function calculateBarWidth(value) {
     const maxWidth = 150;
     const maxStatValue = 255;
@@ -196,37 +153,25 @@ function calculateBarWidth(value) {
     return (percentage / 100) * maxWidth;
 }
 
-function showGeneralStats(index) {
-    document.getElementById('modalContent').innerHTML = "";
-    document.getElementById('modalContent').innerHTML = `
-    <ul class="modal-attributes">
-        <li>
-            <h3 class="attribute-title">Index:</h3>
-            <p>${allPokemonInfos[index].id}</p>
-        </li>
-        <li>
-            <h3 class="attribute-title">Base XP:</h3>
-            <p>${allPokemonInfos[index].base_experience}</p>
-        </li>
-        <li>
-            <h3 class="attribute-title">Height:</h3>
-            <p>${allPokemonInfos[index].height}</p>
-        </li>
-        <li>
-            <h3 class="attribute-title">Weight:</h3>
-            <p>${allPokemonInfos[index].weight}</p>
-        </li>
-        <li>
-            <h3 class="attribute-title">Types:</h3>
-            <p>${allPokemonInfos[index].types.map(typeInfo => typeInfo.type.name).join(', ')}</p>
-        </li>
-    </ul>
-    `;
+function playCry(index) {
+    let cryUrl = allPokemonInfos[index].cries.latest;
+    if (cryUrl) {
+        let audio = new Audio(cryUrl);
+        audio.volume = 0.5;
+        audio.play();
+    } else {
+        console.log("no cry avaliable");
+    }
 }
 
-function showEvolutionChain(index) {
-    document.getElementById('modalContent').innerHTML = "";
-    document.getElementById('modalContent').innerHTML = `
-        <p>Test</p>
-    `
+function isPokemonLegendary(index) {
+    if (allPokemonSpecies[index].is_legendary || allPokemonSpecies[index].is_mythical) {
+        return "legendary";
+    }
+}
+
+function isPokemonLegendaryTitle(index) {
+    if (allPokemonSpecies[index].is_legendary || allPokemonSpecies[index].is_mythical) {
+        return "legendary-name";
+    }
 }
--- templates/template.js
+++ templates/template.js
@@ -5,9 +5,10 @@ function renderPokemonCards(offset) {
         const imgSrc = `https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${pokemon.id}.png`;
         card.classList.add("pokemon-card");
         card.classList.add(`${pokemon.types[0].type.name}`);
+        card.classList.add(`${isPokemonLegendary(i)}`);
         card.dataset.name = pokemon.name;
         card.innerHTML += `
-            <h3>${pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</h3>
+            <h3 class="${isPokemonLegendaryTitle(i)}">${pokemon.name.charAt(0).toUpperCase() + pokemon.name.slice(1)}</h3>
             <p>#${pokemon.id}</p>
             <img class="main-pic" src="${imgSrc}" alt="${pokemon.name}">
             <div class="types">${pokemon.types.map(types => `<div class="type-info ${types.type.name}">${types.type.name}</div>`).join("")}</div>
@@ -17,4 +18,93 @@ function renderPokemonCards(offset) {
         });
         pokemonContainer.appendChild(card);
     }
+}
+
+function renderModal(index) {
+    return pokemonDetailModal.innerHTML = `
+        <div class="pok-detail-content " id="pokemonDetailContent">
+            <div id="pokemonDetails">
+                <h2>${allPokemonInfos[index].name}</h2>
+                <img class="modal-img"src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${allPokemonInfos[index].id}.png" alt="${allPokemonInfos[index].name}">
+                <div id="general-properties" class="">
+                <div class="modal-infos ${allPokemonInfos[index].types[0].type.name}">
+                    <ul class="modal-categories">
+                        <li onclick="showGeneralStats(${index}), playCry(${index})">General</li>
+                        <li onclick="showAttributes(${index})">Attributes</li>
+                        <li onclick="showEvolutionChain(${index})">Evolution</li>
+                    </ul>
+                    <div id="modalContent"></div>
+                </div>
+            </div>
+            <button id="prevPok">Previous</button>
+            <button id="nextPok">Next</button>
+        </div>`;
+};
+
+function showAttributes(index) {
+    document.getElementById('modalContent').innerHTML = "";
+    const pokStats = allPokemonInfos[index].stats;
+    document.getElementById('modalContent').innerHTML = `
+    <ul class="modal-attributes">
+        <li>
+            <h3 class="attribute-title">Health:</h3>
+            <div class="attribute-bar" style="width: ${calculateBarWidth(pokStats[0].base_stat)}px;"></div>
+        </li>
+        <li>
+            <h3 class="attribute-title">Attack:</h3>
+            <div class="attribute-bar" style="width: ${calculateBarWidth(pokStats[1].base_stat)}px;"></div>
+        </li>
+        <li>
+            <h3 class="attribute-title">Defense:</h3>
+            <div class="attribute-bar" style="width: ${calculateBarWidth(pokStats[2].base_stat)}px;"></div>
+        </li>
+        <li>
+            <h3 class="attribute-title">Special-Atk:</h3>
+            <div class="attribute-bar" style="width: ${calculateBarWidth(pokStats[3].base_stat)}px;"></div>
+        </li>
+        <li>
+            <h3 class="attribute-title">Special-Def:</h3>
+            <div class="attribute-bar" style="width: ${calculateBarWidth(pokStats[4].base_stat)}px;"></div>
+        </li>
+        <li>
+            <h3 class="attribute-title">Speed:</h3>
+            <div class="attribute-bar" style="width: ${calculateBarWidth(pokStats[5].base_stat)}px;"></div>
+        </li>
+    </ul>
+    `;
+}
+
+function showGeneralStats(index) {
+    document.getElementById('modalContent').innerHTML = "";
+    document.getElementById('modalContent').innerHTML = `
+    <ul class="modal-attributes">
+        <li>
+            <h3 class="attribute-title">Index:</h3>
+            <p>${allPokemonInfos[index].id}</p>
+        </li>
+        <li>
+            <h3 class="attribute-title">Base XP:</h3>
+            <p>${allPokemonInfos[index].base_experience}</p>
+        </li>
+        <li>
+            <h3 class="attribute-title">Height:</h3>
+            <p>${allPokemonInfos[index].height}</p>
+        </li>
+        <li>
+            <h3 class="attribute-title">Weight:</h3>
+            <p>${allPokemonInfos[index].weight}</p>
+        </li>
+        <li>
+            <h3 class="attribute-title">Types:</h3>
+            <p>${allPokemonInfos[index].types.map(typeInfo => typeInfo.type.name).join(', ')}</p>
+        </li>
+    </ul>
+    `;
+}
+
+function showEvolutionChain(index) {
+    document.getElementById('modalContent').innerHTML = "";
+    document.getElementById('modalContent').innerHTML = `
+        <p>Test</p>
+    `
 }
